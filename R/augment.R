deeeb876e9c2befabf7eec1a30b83040604f0412
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-04-02 20:35:09 -0700

    fix augment and glance for alt models

--- R/augment.R
+++ R/augment.R
@@ -23,23 +23,41 @@ augment.rma <- function(x, ...) {
 
 
  y <- x$yi
- pred <- blup0(x) %>% as.data.frame()
- names(pred) <- c(".fitted", ".se.fit", "conf.low.fit", "conf.high.fit")
+ pred <- blup0(x)
+ if (is.null(pred)) pred <- predict(x)
+ pred <- as.data.frame(pred)
+
+ # fix names
+ names(pred)[1:4] <- c(".fitted", ".se.fit", "conf.low.fit", "conf.high.fit")
+ credible_intervals <- names(pred) %in% c("cr.lb", "cr.ub")
+ names(pred)[credible_intervals] <- c("cred.low.fit", "cred.high.fit")
+ moderator <- names(pred) == "X"
+ names(pred)[moderator] <- "moderator"
+
  res <- residuals0(x)
  inf <- influence0(x)
- inf <- cbind(as.data.frame(inf$inf), dfbetas = inf$dfbs$intrcpt)
- ret <- cbind(.rownames = x$slab,
-       y,
-       pred,
-       .resid = res,
-       dplyr::select(inf, .hat = hat, .cooksd = cook.d, .std.resid = rstudent,
-                     .dffits = dffits, .cov.ratio = cov.r,
-                     tau.squared.del = tau2.del, qe.del = QE.del,
-                     .weight = weight, .dfbetas = dfbetas)
-      )
-  row.names(ret) <- NULL
+ if (!is.null(inf)) {
+   inf <- cbind(as.data.frame(inf$inf), dfbetas = inf$dfbs$intrcpt)
+   inf <- dplyr::select(
+     inf, .hat = hat, .cooksd = cook.d, .std.resid = rstudent,
+     .dffits = dffits, .cov.ratio = cov.r,
+     tau.squared.del = tau2.del, qe.del = QE.del,
+     .weight = weight, .dfbetas = dfbetas)
+ }
+
+ ret <- cbind(
+   .rownames = x$slab,
+   y,
+   pred,
+   .resid = res
+ )
+
+ ret <- tibble::as_tibble(ret) %>%
+   tibble::remove_rownames()
+
   if (all(ret$.rownames == seq_along(ret$.rownames))) {
     ret$.rownames <- NULL
   }
+
   ret
 }
