f22806c42d0c2f8ec977813ee115300745fd0d5a
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-04-02 18:37:41 -0700

    fix tidy() issue with multiple estimates, return tibble

--- R/tidy.R
+++ R/tidy.R
@@ -31,12 +31,24 @@ tidy.rma <- function(x, conf.int = TRUE, exponentiate = FALSE,
                      estimates[, c("ci.lb", "ci.ub")], stringsAsFactors = FALSE)
   names(estimates) <- c("study", "type", "estimate", "std.error", "statistic",
                         "p.value", "conf.low", "conf.high")
+  estimates <- tibble::as_tibble(estimates)
 
-  results <- data.frame(study = "Overall", type = "summary",
-                          estimate = x$beta[1], std.error = x$se,
+  betas <- x$beta
+  if (nrow(betas) > 1) {
+    # get estimate type and fix spelling
+    study <- rownames(betas)
+    swap <- grepl("intrcpt", study)
+    study[swap] <- "intercept"
+    betas <- as.double(betas)
+  } else {
+    study <- "overall"
+    betas <- betas[1]
+  }
+
+  results <- tibble::tibble(study = study, type = "summary",
+                          estimate = betas, std.error = x$se,
                           statistic = x$zval, p.value = x$pval,
-                          conf.low = x$ci.lb, conf.high = x$ci.ub,
-                          stringsAsFactors = FALSE)
+                          conf.low = x$ci.lb, conf.high = x$ci.ub)
   .data <- if (include_studies) rbind(estimates, results) else results
 
   if (exponentiate) {
